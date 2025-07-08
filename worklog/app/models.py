5d6947ad3dc35e0062e07329d445c01ec2319741
Joice Joseph <joice.joseph3198@gmail.com>
2025-07-08 22:26:28 +0530

    stops tracking venv, .env

---
+++ worklog/app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    user_id = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    tag = models.CharField(max_length=100, null=True, blank=True)
+    date = models.DateField()
+    #Audit Fields
+    active = models.BooleanField(default=True)
+    deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return (
+            f"Task(user_id={self.user_id}, "
+            f"title={self.title}, "
+            f"description={self.description}, "
+            f"tag={self.tag}, "
+            f"active={self.active}, "
+            f"deleted={self.deleted}, "
+            f"created_at={self.created_at}, "
+            f"updated_at={self.updated_at})"
+    )
